e439614ce06fda97adefb38420037b00006a594a
Annargb <annasamoylenko2015@gmail.com>
2024-01-11 15:47:06 +0100

    add all

---
+++ schemas/contacts.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.number().integer().required(),
+});
+
+module.exports = addSchema;
